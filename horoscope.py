626b8176f4b2a0bbe3a193686da35f8eea2fc59e
Michael Sproul <micsproul@gmail.com>
2013-12-04 14:49:55 +1100

    Added two more sentence structures

--- horoscope.py
+++ horoscope.py
@@ -2,10 +2,36 @@ import random
 
 from wordlist import *
 
-def sanitize(sentence):
-        return sentence[0].upper() + sentence[1:] + "."
+def sentence_case(sentence, exciting=False):
+	if exciting:
+		return "%s%s!" %(sentence[0].upper(), sentence[1:])
+	else:
+		return "%s%s." %(sentence[0].upper(), sentence[1:])
 
-def prediction_sentence():
+def horoscope():
+	sentences = [statement_s, cosmic_implication_s, plain_warning_s]
+	random.shuffle(sentences)
+	final_text = ""
+	for sentence in sentences:
+		final_text += sentence() + " "
+	return final_text.rstrip()
+
+def statement_s():
+	s = "You are feeling %s %s" % (choose(emotional_degree), choose(good_feeling_adj))
+	s += ", and there's nothing anyone can %s to stop you" % choose(["say", "do"])
+	return sentence_case(s, exciting=True)
+
+def plain_warning_s():
+	r = random.random()
+
+	if r <= 0.5:
+		s = "You would be well advised to avoid " + choose(avoid)
+	else:
+		s = "Avoid " + choose(avoid) + "; you wouldn't want to sabotage your mental wellbeing"
+
+	return sentence_case(s)
+
+def cosmic_implication_s():
 	s = cosmic_event()
 	s += " " + choose(prediction_verb)
 	s += " the " + choose(time_point)
@@ -16,7 +42,7 @@ def prediction_sentence():
 	else:
 		s += " of a " + event
 
-	return sanitize(s)
+	return sentence_case(s)
 
 def cosmic_event():
 	r = random.random()
@@ -29,6 +55,8 @@ def cosmic_event():
 		return ce
 	elif r <= 0.6:
 		return "the " + choose(["New", "Full"]) + " Moon"
+	elif r <= 0.75:
+		return choose(wanky_event)
 	else:
 		first = choose(planet, star, ["Moon"])
 		second = choose_uniq({first}, planet, star, ["Moon"])
@@ -38,9 +66,11 @@ def emotive_event():
 	r = random.random()
 
 	if r <= 0.5:
-		return choose(emotive_adj) + " " + choose(time_period)
+		adj = choose(good_feeling_adj, good_emotive_adj, bad_emotive_adj)
+		return adj + " " + choose(time_period)
 	else:
-		return choose(time_period) + " of " + choose(feeling)
+		noun = choose(good_emotive_noun, bad_emotive_noun)
+		return choose(time_period) + " of " + noun
 
 def choose_uniq(exclude, *args):
 	item = choose(*args)
--- wordlist.py
+++ wordlist.py
@@ -1,4 +1,4 @@
-# Cosmic event words
+# Astrological words
 planet = ["Mercury", "Venus", "Mars", "Jupiter", "Saturn",
 		"Uranus", "Neptune", "Pluto"]
 
@@ -6,9 +6,11 @@ star = ["Proxima Centauri", "Barnard's Star", "Sirius A", "Epsilon Eridani"]
 
 aspect = ["conjunction", "sextile", "square", "trine", "opposition"]
 
+wanky_event = ["a large Electromagnetic disturbance", "Quantum Flux", "the upcoming Solar eclipse",
+		"Unusual planetary motion"]
 
 # Prediction words
-prediction_verb = ["heralds", "marks", "foreshadows"]
+prediction_verb = ["heralds", "marks", "foreshadows", "signifies"]
 
 # Time related words
 time_point = ["arrival", "beginning", "start", "end", "death"]
@@ -16,11 +18,26 @@ time_point = ["arrival", "beginning", "start", "end", "death"]
 time_period = ["interlude", "period", "week", "day"]
 
 # Emotive words
-emotive_adj = ["romantic", "cathartic", "healing", "mystical", "enlightening",
-		"emotional", "reflective", "irreverent", "subversive"]
+good_feeling_adj = ["romantic", "emotional", "reflective", "irreverent", "subversive",
+			"spiritual", "creative", "intellectual", "adventurous"]
 
-feeling = ["love", "bitterness", "reflection", "romance", "enlightenment", "healing",
-		"catharsis", "mysticism", "sadness", "joy", "desire"]
+good_emotive_adj = ["cathartic", "healing", "enlightening", "mystical"]
+
+bad_emotive_adj = ["bitter", "anti-climactic", "disappointing", "emotional", "frustrating"]
+
+good_emotive_noun = ["love", "reflection", "romance", "enlightenment",
+			"healing", "catharsis", "mysticism", "joy", "desire",
+			"transcendence", "creativity", "metamorphosis"]
+
+bad_emotive_noun = ["bitterness", "disappointment", "sadness", "frustration", "anger", "failure", "boredom"]
+
+emotional_degree = ["a little bit", "pretty", "ridiculously", "amazingly"]
 
 # Misc
 vowels = {'a', 'e', 'i', 'o', 'u'}
+
+# "You would be well advised to avoid..."
+avoid = ["shopping", "engaging strangers in conversation", "swimming",
+		"making too many jokes", "eating seafood", "rigorous physical activity",
+		"staying inside for extended periods of time", "alienating your friends",
+		"making life-changing decisions", "prolonging the inevitable"]
