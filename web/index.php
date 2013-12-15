a9ed2cce2dbcf324d81762df77d413520320ed1b
Michael Sproul <micsproul@gmail.com>
2013-12-15 17:17:00 +1100

    Added a title to the webpage

--- web/index.php
+++ web/index.php
@@ -1,8 +1,10 @@
+<!doctype html>
+<html>
+<head>
+<title>
+Horoscope Generator | Statistically Insignificant Wisdom
+</title>
 <style>
-html, body {
-	padding: 0;
-	margin: 0;
-}
 html {
 	min-width: 100%;
 	min-height: 100%;
@@ -12,17 +14,20 @@ html {
 	background-repeat: no-repeat;
 }
 body {
-	width: 70%;
-	font-family: arial;
-	color: white;
 	background-color: rgba(50, 50, 50, .6);
+	width: 70%;
 	margin: 5% auto;
 	padding: 20px;
 	padding-top: 100px;
 	padding-bottom: 100px;
 	border-radius: 10px;
 	text-align: center;
+	font-family: arial;
+	color: white;
 }
 </style>
-
+</head>
+<body>
 <h1><?php echo exec('python horoscope.py'); ?></h1>
+</body>
+</html>
